0771b5392355499d8d1bc7a713f4853586ea6150
qujinwei <157279031@qq.com>
2021-03-04 19:28:46 +0800

    fetch封装

---
+++ fetch/fetch.js
@@ -0,0 +1,55 @@
+const isObject = function(o){
+    return Object.prototype.toString(o) === "[object Object]"
+}
+
+class FetchRequest {
+    constructor(baseUrl = ''){
+        this.baseUrl = baseUrl
+    }
+
+    getInstanceConfig(){
+        const config = {
+            baseConfig: this.baseUrl,
+            headers: {
+                
+            },
+            withCredentials: true 
+        }
+        return config
+    }
+    setOption(options){
+        options = Object.assign(this.getInsideConfig(), options)
+        let url = this.baseUrl + options.url;
+        options.method = options.method ? options.method.toUpperCase() : 'GET'
+        if(options.params && isObject(options.params)){
+            if(/^(GET|DELETE|HEAD|OPTIONS)$/i.test(options.method)){
+                params._timeStamp = new Date().getTime()
+                url += url.includes('?') ? '' : '?'
+                params.keys.forEach((key, i) => {
+                    url += `${i > 0 ? '&' : ''}${key}=${params[key]}`
+                });
+            }else if(/^(POST|PUT)$/i.test(options.method)){
+                if(options.headers && options.headers["Content-Type"] === "application/json"){
+                    try {
+                        options.body = JSON.stringify(options.body)
+                    } catch (error) {}
+                }
+            }
+            delete options.params
+        }
+        return options
+    }
+    instance(options){
+        return fetch(options.url, options).then(response => {
+            
+        }).catch(error => {
+
+        })
+    }
+
+    request(options = {}){
+        options = this.setOption(options)
+        
+        return this.instance(options)
+    }
+}
